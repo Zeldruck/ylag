5ee8837da556a6307bc8195b76bbb4fcd80c5fc1
Zeldruck <felix.jouannneau@gmail.com>
2024-04-02 19:21:42 -0400

    Add navigation tab in project

--- app/src/main/java/com/zeldruck/ylag/MainActivity.kt
+++ app/src/main/java/com/zeldruck/ylag/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.zeldruck.ylag
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.zeldruck.ylag.presentation.calendar.CalendarScreen
+import com.zeldruck.ylag.presentation.home.HomeScreen
+import com.zeldruck.ylag.presentation.nav.components.NavigationBar
+import com.zeldruck.ylag.presentation.profile.ProfileScreen
 import com.zeldruck.ylag.ui.theme.YourLifeAsAGameTheme
 
 
@@ -23,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    DefaultPreview()
                 }
             }
         }
@@ -35,10 +43,42 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
-    YourLifeAsAGameTheme {
-        Greeting("Android")
+    val navController = rememberNavController()
+
+    Scaffold(bottomBar = { NavigationBar(navController) }) {
+        MainScreenNavigationConfigurations(navController = navController)
+    }
+}
+
+@Composable
+private fun MainScreenNavigationConfigurations(
+    navController: NavHostController
+) {
+    NavHost(navController = navController, startDestination = "Home")
+    {
+        composable("Home")
+        {
+            HomeScreen();
+        }
+
+        composable("Schedule")
+        {
+            CalendarScreen()
+        }
+
+        composable("Achievements")
+        {
+            HomeScreen()
+        }
+
+        composable("Profile")
+        {
+            ProfileScreen()
+        }
     }
 }
--- app/src/main/java/com/zeldruck/ylag/presentation/nav/components/NavigationBar.kt
+++ app/src/main/java/com/zeldruck/ylag/presentation/nav/components/NavigationBar.kt
@@ -1,31 +1,32 @@
 package com.zeldruck.ylag.presentation.nav.components
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.zeldruck.ylag.presentation.nav.cases
 
-@Preview
 @Composable
-fun NavigationBar() {
-    var selectedItem by remember { mutableStateOf(0) }
-
-    androidx.compose.material3.NavigationBar() {
-        cases.forEachIndexed{ index, item ->
+fun NavigationBar(navController: NavHostController) {
+    androidx.compose.material3.NavigationBar {
+        val currentRoute = currentRoute(navController)
+        cases.forEachIndexed{ _, item ->
             NavigationBarItem(
                 icon = { Icon(item.icon, contentDescription = item.name) },
                 label = { Text(item.name) },
-                selected = selectedItem == index,
-                onClick = { selectedItem = index }
+                selected = currentRoute == item.name,
+                alwaysShowLabel = false,
+                onClick = { if (currentRoute != item.name) navController.navigate(item.name)}
             )
         }
     }
+}
+
+@Composable
+private fun currentRoute(navController: NavHostController): String? {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    return navBackStackEntry?.arguments?.getString("KEY_ROUTE")
 }
